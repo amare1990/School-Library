ba411feb3df834f681848f569b68476c96c5dba8
amare1990 <amaremek@gmail.com>
2023-01-02 23:11:31 +0300

    Add Student class

---
+++ student.rb
@@ -0,0 +1,14 @@
+require "./person.rb"
+
+class Student < Person
+
+  def initialize(name, age, parent_permission, classroom)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+end
