84a7d4a71692bc7463e974ea3abd5d8f0468f115
amare1990 <amaremek@gmail.com>
2023-01-03 01:25:47 +0300

    Correct feedback from the code reviewer

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require './person'
 
 class Teacher < Person
-  def initialize(age, parent_permission, name, specialization)
-    super(name, age, parent_permission)
+  def initialize(specialization, age, name = "Unknown", parent_permission:true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
