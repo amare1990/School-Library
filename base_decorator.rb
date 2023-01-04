f5c95138398d3acfd39e6e742e2fd258a9daaf1d
amare1990 <amaremek@gmail.com>
2023-01-04 11:26:56 +0300

    Add base decorator class

---
+++ base_decorator.rb
@@ -0,0 +1,13 @@
+require './nameable'
+
+class Decorator < Nameable
+  def initialize (decorator)
+    super()
+    @nameable = decorator
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+
+end
