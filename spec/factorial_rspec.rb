ed311caeed002e16388925710f9b620c94e30ed5
amare1990 <amaremek@gmail.com>
2023-01-13 01:22:25 +0300

    Add tests for the factorial of 0

--- spec/factorial_rspec.rb
+++ spec/factorial_rspec.rb
@@ -8,4 +8,12 @@ describe Solver do
       expect(factorial).to eq 120
     end
   end
+
+  context 'When testing the Solver class' do
+    it 'The factorial of 0 should be 1' do
+      solver = Solver.new
+      factorial = solver.factorial(0)
+      expect(factorial).to eq 1
+    end
+  end
 end
