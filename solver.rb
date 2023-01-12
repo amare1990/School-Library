99e092c8b39d0340be66910ff04c7d9452b87f65
amare1990 <amaremek@gmail.com>
2023-01-13 00:14:50 +0300

    Implement the fizzbuzz method code

--- solver.rb
+++ solver.rb
@@ -16,4 +16,22 @@ class Solver
   def reverse(word)
     word = word.reverse
   end
+
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      return "fizzbuzz"
+    elsif n % 3 == 0
+      return "fizz"
+    elsif n % 5 == 0
+      return "buzz"
+    else
+      return n.to_s
+    end
+  end
+
+
+
 end
+
+
+
--- spec/fizzbuzz_rspec.rb
+++ spec/fizzbuzz_rspec.rb
@@ -9,28 +9,28 @@ describe Solver do
   context 'When testing the Solver class' do
     it 'Should return fizz when n is 3' do
 
-      expect(@solver.fizzbuz(3)).to eq "fizz"
+      expect(@solver.fizzbuzz(3)).to eq "fizz"
     end
   end
 
   context 'When testing the Solver class' do
     it 'Should return buzz when n is 5' do
 
-      expect(@solver.fizzbuz(5)).to eq "buzz"
+      expect(@solver.fizzbuzz(5)).to eq "buzz"
     end
   end
 
   context 'When testing the Solver class' do
     it 'Should return fizzbuzz when n is 15' do
 
-      expect(@solver.fizzbuz(15)).to eq "fizzbuzz"
+      expect(@solver.fizzbuzz(15)).to eq "fizzbuzz"
     end
   end
 
   context 'When testing the Solver class' do
     it 'Should return 7 when n is 7' do
 
-      expect(@solver.fizzbuz(7)).to eq 7
+      expect(@solver.fizzbuzz(7)).to eq "7"
     end
   end
 
