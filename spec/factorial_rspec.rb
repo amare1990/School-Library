c0411cc54877a4a45a5db375cfafdd4452c881b1
amare1990 <amaremek@gmail.com>
2023-01-13 01:38:54 +0300

    Implent unit testing example when the parameter of factorial is -ve integers

--- spec/factorial_rspec.rb
+++ spec/factorial_rspec.rb
@@ -16,4 +16,11 @@ describe Solver do
       expect(factorial).to eq 1
     end
   end
+
+  context 'When testing the Solver class' do
+    it 'raises an exception when given a negative value' do
+      solver = Solver.new
+      expect { solver.factorial(-1) }.to raise_error('Factorial of a negative number is not applicable')
+    end
+  end
 end
