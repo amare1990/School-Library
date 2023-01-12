4e4da91b0a395f612ca0f5a13500815fd7a7903b
amare1990 <amaremek@gmail.com>
2023-01-13 00:08:59 +0300

    Implement the unit test for the fizzbuzz method

---
+++ spec/fizzbuzz_rspec.rb
@@ -0,0 +1,37 @@
+require '../solver'
+
+describe Solver do
+
+  before(:all) do
+    @solver = Solver.new
+  end
+
+  context 'When testing the Solver class' do
+    it 'Should return fizz when n is 3' do
+
+      expect(@solver.fizzbuz(3)).to eq "fizz"
+    end
+  end
+
+  context 'When testing the Solver class' do
+    it 'Should return buzz when n is 5' do
+
+      expect(@solver.fizzbuz(5)).to eq "buzz"
+    end
+  end
+
+  context 'When testing the Solver class' do
+    it 'Should return fizzbuzz when n is 15' do
+
+      expect(@solver.fizzbuz(15)).to eq "fizzbuzz"
+    end
+  end
+
+  context 'When testing the Solver class' do
+    it 'Should return 7 when n is 7' do
+
+      expect(@solver.fizzbuz(7)).to eq 7
+    end
+  end
+
+end
